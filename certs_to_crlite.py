d62b16448eb1dff9eea318790deb62662266709f
groovecoder <luke.crouch@gmail.com>
2018-03-19 15:53:36 -0500

    start with a tiny DigiCert-only mlbf

--- certs_to_crlite.py
+++ certs_to_crlite.py
@@ -1,26 +1,42 @@
+import censys.certificates
+from decouple import config
 from filter_cascade import FilterCascade
+import sys
 
+CENSYS_API_UID = config("CENSYS_API_UID", None)
+CENSYS_API_SECRET = config("CENSYS_API_SECRET", None)
+
+if not CENSYS_API_UID or not CENSYS_API_SECRET:
+    print "Must set CENSYS_API_UID and CENSYS_API_SECRET"
+    sys.exit(1)
+
+certificates = censys.certificates.CensysCertificates(
+    CENSYS_API_UID, CENSYS_API_SECRET
+)
 
 MLBF_FILENAME = 'moz-crlite-mlbf'
-
-counter = 0
 
 a = []
 b = []
 
-wordfile = open('/usr/share/dict/words')
+certs_list = certificates.search(
+    'parsed.issuer.organization.raw: "DigiCert Inc"'
+)
 
-for word in wordfile.readlines():
-    # let's say every third word should *not* be in the filter
-    shouldAdd = (counter % 3 == 1)
-    if shouldAdd:
-        a.append(word)
+for idx, c in enumerate(certs_list):
+    print "Iteration %i" % idx
+    if idx == 500:
+        break
+    # let's say every third cert should *not* be in the filter
+    shouldBeRevoked = (idx % 3 == 1)
+    if shouldBeRevoked:
+        print "Appending %s to revoked list." % c
+        a.append(c["parsed.fingerprint_sha256"])
     else:
-        b.append(word)
-    counter = counter + 1
-wordfile.close()
+        print "Appending %s to okay list." % c
+        b.append(c["parsed.fingerprint_sha256"])
 
-cascade = FilterCascade(50000, 1.3, 0.77, 1)
+cascade = FilterCascade(500, 1.3, 0.77, 1)
 cascade.initialize(a, b)
 cascade.check(a, b)
 
