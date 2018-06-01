f7aba5ae71e300e37915ad19c2d6d55934795cd6
Lesley Norton <lesley@lesleynorton.com>
2018-06-01 15:27:02 -0500

    start disconnect mapping

---
+++ disconnect-mapping.py
@@ -0,0 +1,63 @@
+disconnect_mapping = {
+
+  #google
+  
+  "developers.google.com": "Social",
+  "gmail.com": "Social",
+  "mail.google.com": "Social",
+  "inbox.google.com": "Social",
+  "orkut.com": "Social",
+  "plus.google.com": "Social",
+  "plusone.google.com": "Social",
+  "voice.google.com": "Social",
+  "googlemail.com": "Social",
+
+  "google-analytics.com": "Analytics",
+  "postrank.com": "Analytics",
+
+  "2mdn.net": "Advertising",
+  "admeld.com": "Advertising",
+  "admob.com": "Advertising",
+  "cc-dt.com": "Advertising",
+  "destinationurl.com": "Advertising",
+  "doubleclick.net": "Advertising",
+  "adwords.google.com": "Advertising",
+  "googleadservices.com": "Advertising",
+  "googlesyndication.com": "Advertising",
+  "googletagservices.com": "Advertising",
+  "invitedmedia.com": "Advertising",
+  "smtad.net": "Advertising",
+  "terracent.com": "Advertising",
+  "terracent.net": "Advertising",
+  "ytsa.net": "Advertising",
+  
+
+  
+  
+  #facebook
+
+  "facebook.com": "Social",
+  "facebook.de": "Social",
+  "facebook.fr": "Social",
+  "facebook.net": "Social",
+  "fb.com": "Social",
+  "friendfeed.com": "Social",
+
+  "atlassolutions.com": "Advertising",
+
+
+  
+  
+  #twitter 
+
+  "twitter.com": "Social",
+  "tiwtter.jp": "Social",
+  "twimg.com": "Social",
+
+  #now kicking to a godaddy default / looks abandoned
+  "backtype.com": "Social",
+  #do we leave or remove?
+
+  "crashlytics.com": "Analytics",
+  "tweetdeck.com": "Analytics"
+}
