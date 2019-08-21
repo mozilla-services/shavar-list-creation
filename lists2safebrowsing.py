2ac7d7a582597d9f5fb3ad7cf7a27d0016034959
Se Yeon Kim <25109943+skim1102@users.noreply.github.com>
2019-08-21 11:43:11 -0500

    Move constants to constants file

--- lists2safebrowsing.py
+++ lists2safebrowsing.py
@@ -18,118 +18,28 @@ from publicsuffixlist.update import updatePSL
 
 from trackingprotection_tools import DisconnectParser
 
+from constants import (
+    ALL_TAGS,
+    DEFAULT_DISCONNECT_LIST_CATEGORIES,
+    DEFAULT_DISCONNECT_LIST_TAGS,
+    DNT_BLANK_SECTIONS,
+    DNT_CONTENT_SECTIONS,
+    DNT_EFF_SECTIONS,
+    DNT_SECTIONS,
+    DNT_W3C_SECTIONS,
+    FASTBLOCK_SECTIONS,
+    PLUGIN_SECTIONS,
+    PRE_DNT_CONTENT_SECTIONS,
+    PRE_DNT_SECTIONS,
+    TEST_DOMAIN_TEMPLATE,
+    WHITELIST_SECTIONS,
+
+)
 updatePSL()
 psl = PublicSuffixList()
 
 DISCONNECT_MAPPING = os.path.join(
     os.path.dirname(__file__), 'disconnect_mapping.json')
-
-PLUGIN_SECTIONS = (
-    "plugin-blocklist",
-    "plugin-blocklist-experiment",
-    "flash-blocklist",
-    "flash-exceptions",
-    "flash-allow",
-    "flash-allow-exceptions",
-    "flash-subdoc",
-    "flash-subdoc-exceptions",
-    "flashinfobar-exceptions"
-)
-WHITELIST_SECTIONS = (
-    "entity-whitelist",
-    "entity-whitelist-testing",
-    "staging-entity-whitelist",
-    "fastblock1-whitelist",
-    "fastblock2-whitelist"
-)
-PRE_DNT_SECTIONS = (
-    "tracking-protection",
-    "tracking-protection-testing",
-    "tracking-protection-standard",
-    "tracking-protection-full",
-    "staging-tracking-protection-standard",
-    "staging-tracking-protection-full",
-    "fanboy-annoyance",
-    "fanboy-social",
-    "easylist",
-    "easyprivacy",
-    "adguard",
-    "social-tracking-protection",
-    "social-tracking-protection-facebook",
-    "social-tracking-protection-twitter",
-    "social-tracking-protection-linkedin",
-    "social-tracking-protection-youtube",
-)
-PRE_DNT_CONTENT_SECTIONS = (
-    "tracking-protection-full",
-    "staging-tracking-protection-full"
-)
-DNT_SECTIONS = (
-    "tracking-protection-base",
-    "tracking-protection-baseeff",
-    "tracking-protection-basew3c",
-    "tracking-protection-content",
-    "tracking-protection-contenteff",
-    "tracking-protection-contentw3c",
-    "tracking-protection-ads",
-    "tracking-protection-analytics",
-    "tracking-protection-social",
-    "tracking-protection-base-fingerprinting",
-    "tracking-protection-content-fingerprinting",
-    "tracking-protection-base-cryptomining",
-    "tracking-protection-content-cryptomining",
-    "tracking-protection-test-multitag",
-    "fastblock1",
-    "fastblock2",
-    "fastblock3"
-)
-DNT_CONTENT_SECTIONS = (
-    "tracking-protection-content",
-    "tracking-protection-contenteff",
-    "tracking-protection-contentw3c"
-)
-DNT_BLANK_SECTIONS = (
-    "tracking-protection-base",
-    "tracking-protection-content",
-)
-DNT_EFF_SECTIONS = (
-    "tracking-protection-baseeff",
-    "tracking-protection-contenteff",
-)
-DNT_W3C_SECTIONS = (
-    "tracking-protection-basew3c",
-    "tracking-protection-contentw3c"
-)
-FASTBLOCK_SECTIONS = (
-    "fastblock1",
-    "fastblock1-whitelist",
-    "fastblock2",
-    "fastblock2-whitelist",
-    "fastblock3"
-)
-
-FINGERPRINTING_TAG = 'fingerprinting'
-CRYPTOMINING_TAG = 'cryptominer'
-SESSION_REPLAY_TAG = 'session-replay'
-PERFORMANCE_TAG = 'performance'
-ALL_TAGS = {
-    FINGERPRINTING_TAG,
-    CRYPTOMINING_TAG,
-    SESSION_REPLAY_TAG,
-    PERFORMANCE_TAG
-}
-
-TEST_DOMAIN_TEMPLATE = '%s.dummytracker.org'
-
-DEFAULT_DISCONNECT_LIST_CATEGORIES = [
-    'Advertising|Analytics|Social|Disconnect']
-DEFAULT_DISCONNECT_LIST_TAGS = {}
-
-CONFIG = ConfigParser.ConfigParser()
-FILENAME = CONFIG.read(["shavar_list_creation.ini"])
-REMOTE_SETTING_URL = CONFIG.get('main', 'remote_setting_url')
-REMOTE_SETTING_BUCKET = CONFIG.get('main', 'remote_setting_bucket')
-REMOTE_SETTING_COLLECTION = CONFIG.get('main', 'remote_setting_collection')
 
 
 def get_output_and_log_files(config, section):
