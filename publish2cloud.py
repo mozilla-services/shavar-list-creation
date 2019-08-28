5b0d27057c1090c5b1df0f9c033c132836b900d4
Se Yeon Kim <25109943+skim1102@users.noreply.github.com>
2019-08-28 14:58:35 -0500

    Set default values when environment var is not available

--- publish2cloud.py
+++ publish2cloud.py
@@ -18,13 +18,18 @@ from constants import (
 
 CONFIG = ConfigParser.SafeConfigParser(os.environ)
 CONFIG.read(['shavar_list_creation.ini'])
-REMOTE_SETTINGS_URL = CONFIG.get('main', 'remote_settings_url')
+REMOTE_SETTINGS_URL = ''
+if os.environ.get('SHAVAR_REMOTE_SETTINGS_URL', None):
+    REMOTE_SETTINGS_URL = CONFIG.get('main', 'remote_settings_url')
 REMOTE_SETTINGS_BUCKET = CONFIG.get('main', 'remote_settings_bucket')
 REMOTE_SETTINGS_COLLECTION = CONFIG.get('main', 'remote_settings_collection')
 REMOTE_SETTINGS_RECORD_PATH = ('/buckets/{bucket_name}'
                                + '/collections/{collection_name}/records')
-REMOTE_SETTINGS_AUTH = (CONFIG.get('main', 'remote_settings_username'),
-                        CONFIG.get('main', 'remote_settings_password'))
+REMOTE_SETTINGS_AUTH = ('', '')
+if (os.environ.get('SHAVAR_REMOTE_SETTINGS_USERNAME', None)
+    and os.environ.get('SHAVAR_REMOTE_SETTINGS_PASSWORD', None)):
+        REMOTE_SETTINGS_AUTH = (CONFIG.get('main', 'remote_settings_username'),
+                                CONFIG.get('main', 'remote_settings_password'))
 
 
 def chunk_metadata(fp):
