69d8a35e0b1dc3aefb1d599352bad771e5daf250
Georgia Kokkinou <geor5ko@gmail.com>
2020-07-10 14:59:10 +0300

    Add unit tests for get_output_and_log_files

--- tests/test_lists2safebrowsing.py
+++ tests/test_lists2safebrowsing.py
@@ -222,7 +222,7 @@ ENTITY_LIST_URL = ("https://raw.githubusercontent.com/mozilla-services/"
 PLUGIN_LIST_URL = ("https://raw.githubusercontent.com/mozilla-services/"
                    "shavar-plugin-blocklist/master/mozplugin-block.txt")
 
-TEST_SECTION = "tracking-protection-test"
+TEST_SECTION = "tracking-protection"
 
 PRINT_MSG = "%s(%s): publishing %d items; file size %d\n"
 LOG_MSG = "[{}] {} >> (canonicalized) {}, hash {}\n"
@@ -234,8 +234,39 @@ def chunknum():
 
 
 @pytest.fixture
+def config():
+    config = ConfigParser.ConfigParser()
+    config.readfp(open("sample_shavar_list_creation.ini"))
+    return config
+
+
+@pytest.fixture
 def parser():
     return DisconnectParser(blocklist="tests/sample_blocklist.json")
+
+
+def test_get_output_and_log_files(config):
+    """Test getting output and log files from configuration file."""
+    with patch("lists2safebrowsing.open", mock_open()) as mocked_open:
+        output_file, log_file = l2s.get_output_and_log_files(config,
+                                                             TEST_SECTION)
+
+    expected_calls = [call("mozpub-track-digest256", "wb"),
+                      call("mozpub-track-digest256.log", "w")]
+
+    assert output_file is not None
+    assert log_file is not None
+    assert mocked_open.mock_calls == expected_calls
+
+
+def test_get_output_and_log_files_no_filename(config):
+    """Test get_output_and_log_files when filename is not specified."""
+    config.set(TEST_SECTION, "output", "")
+    output_file, log_file = l2s.get_output_and_log_files(config,
+                                                         TEST_SECTION)
+
+    assert output_file is None
+    assert log_file is None
 
 
 def test_canonicalize_return_type():
@@ -551,11 +582,8 @@ def _get_entity_or_plugin_lists(chunknum, config, function, section, data):
     [pytest.param(section, version, id, id=id)
         for id, section, version in GET_ENTITY_LISTS_TESTCASES]
 )
-def test_get_entity_lists(chunknum, section, version, testcase):
+def test_get_entity_lists(config, chunknum, section, version, testcase):
     """Test creating an entity list from a configuration section."""
-    config = ConfigParser.ConfigParser()
-    config.readfp(open("sample_shavar_list_creation.ini"))
-
     if version:
         config.set(section, "version", version)
 
@@ -584,10 +612,8 @@ def test_get_entity_lists(chunknum, section, version, testcase):
     assert output_writes == expected_output_writes
 
 
-def test_get_plugin_lists(chunknum):
+def test_get_plugin_lists(config, chunknum):
     """Test creating a plugin blocklist from a configuration section."""
-    config = ConfigParser.ConfigParser()
-    config.readfp(open("sample_shavar_list_creation.ini"))
     section = "plugin-blocklist"
 
     domains = [d[1] for d in CANONICALIZE_TESTCASES[:2]]
@@ -615,10 +641,8 @@ def test_get_plugin_lists(chunknum):
     assert output_writes == expected_output_writes
 
 
-def test_get_plugin_lists_empty_url(chunknum):
+def test_get_plugin_lists_empty_url(config, chunknum):
     """Test empty blocklist URL handling in get_plugin_lists."""
-    config = ConfigParser.ConfigParser()
-    config.readfp(open("sample_shavar_list_creation.ini"))
     section = "plugin-blocklist"
 
     config.set(section, "blocklist", "")
@@ -632,10 +656,9 @@ def test_get_plugin_lists_empty_url(chunknum):
     [pytest.param(section, domains, id, id=id)
         for id, section, domains in GET_TRACKER_LISTS_TESTCASES]
 )
-def test_get_tracker_lists(parser, chunknum, section, domains, testcase):
+def test_get_tracker_lists(config, parser, chunknum, section, domains,
+                           testcase):
     """Test creating a tracker blocklist from a configuration section."""
-    config = ConfigParser.ConfigParser()
-    config.readfp(open("sample_shavar_list_creation.ini"))
     version = None
 
     if testcase == "default":
